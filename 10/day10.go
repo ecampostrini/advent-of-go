93ed3811eda4cd301ca0500d8d09f04c2bfb69d6
Esteban Campostrini <lesteban22@gmail.com>
2021-12-10 16:35:21 +0100

    Refactor day 10 part 1

--- 10/day10.go
+++ 10/day10.go
@@ -27,33 +27,34 @@ func areAMatch(c1, c2 int) bool {
 	return false
 }
 
+func getFirstMismatch(line string) int {
+	stack, stackPos := make([]int, len(line)), 0
+	for i := 0; i < len(line); i++ {
+		currentChar := int(line[i])
+		if points[currentChar] == 0 {
+			stack[stackPos] = currentChar
+			stackPos++
+		} else {
+			if !areAMatch(stack[stackPos-1], currentChar) {
+				return currentChar
+			}
+			stackPos--
+		}
+	}
+	return 0
+}
+
 func main() {
-	file, err := os.Open("./input.txt")
+	file, err := os.Open("./input.test.txt")
 	if err != nil {
 		fmt.Println("Failed to open input file: ", err)
 		os.Exit(1)
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
 	syntaxErroScore := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		stack, stackPos := make([]int, len(line)), 0
-		currentMismatch := 0
-		for i := 0; currentMismatch == 0 && i < len(line); i++ {
-			currentChar := int(line[i])
-			if points[currentChar] == 0 {
-				stack[stackPos] = currentChar
-				stackPos++
-			} else {
-				if !areAMatch(stack[stackPos-1], currentChar) {
-					currentMismatch = currentChar
-				}
-				stackPos--
-			}
-		}
-		syntaxErroScore += points[currentMismatch]
+	for scanner := bufio.NewScanner(file); scanner.Scan(); {
+		syntaxErroScore += points[getFirstMismatch(scanner.Text())]
 	}
 	fmt.Println(syntaxErroScore)
 }
