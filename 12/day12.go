b3f0733231b1f96475d6ae6923c3b40dd260e243
Esteban Campostrini <lesteban22@gmail.com>
2021-12-12 14:12:00 +0100

    Add solution to day 12 part 1

--- 12/day12.go
+++ 12/day12.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
 type Graph = map[string][]string
@@ -23,13 +24,55 @@ func parseGraph(scanner *bufio.Scanner) Graph {
 	return ret
 }
 
+func isLowerCase(s string) bool {
+	for _, c := range s {
+		if !unicode.IsLower(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func countPaths(graph Graph) int {
+	var helper func(string)
+	var pathCount int
+	currentPath := []string{"start"}
+	visited := make(map[string]bool)
+
+	helper = func(currentNode string) {
+		if currentNode == "end" {
+			pathCount++
+			fmt.Printf("%v\n", currentPath)
+			return
+		}
+
+		if isLowerCase(currentNode) {
+			visited[currentNode] = true
+		}
+		for _, neighbour := range graph[currentNode] {
+			if visited[neighbour] {
+				continue
+			}
+			currentPath = append(currentPath, neighbour)
+			helper(neighbour)
+			currentPath = currentPath[:len(currentPath)-1]
+		}
+		visited[currentNode] = false
+	}
+
+	helper("start")
+
+	return pathCount
+}
+
 func main() {
-	file, err := os.Open("./input.test.txt")
+	file, err := os.Open("./input.txt")
 	if err != nil {
 		fmt.Println("Failed to read input file: ", err)
 		os.Exit(1)
 	}
 	graph := parseGraph(bufio.NewScanner(file))
-
 	fmt.Printf("%v\n", graph)
+
+	fmt.Println(countPaths(graph))
 }
