eb67f0f781c7fa496e6740a623fcfba18d37b767
Esteban Campostrini <lesteban22@gmail.com>
2021-12-14 10:28:17 +0100

    Add solution to day 14, raw

--- 14/day14.go
+++ 14/day14.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "math"
 	"bufio"
 	"fmt"
 	"github.com/ecampostrini/advent-of-go/utils/files"
@@ -17,6 +18,14 @@ func ParseStringMap(scanner *bufio.Scanner, separator string) map[string]string 
 	return ret
 }
 
+func getLength(m map[string]int) int {
+  var ret int
+  for _, count := range m {
+    ret += count
+  }
+  return ret
+}
+
 func main() {
 	scanner, file := files.ReadFile("./input.txt")
 	defer file.Close()
@@ -29,36 +38,42 @@ func main() {
 	insertionRules := ParseStringMap(scanner, "->")
 	//fmt.Println(insertionRules)
 
+	polymerMap := make(map[string]int)
+	for i := 0; i < len(template)-1; i++ {
+		polymerMap[template[i:i+2]]++
+	}
+	fmt.Printf("Polymer map: %v\n", polymerMap)
+
 	occurrenceMap := make(map[string]int)
 	for _, c := range template {
 		occurrenceMap[string(c)]++
 	}
+	fmt.Printf("Occurrence map: %v\n", occurrenceMap)
 
-	for step := 0; step < 40; step++ {
-    fmt.Printf("Step: %d, template size: %d\n", step, len(template))
-		currentPolymer := template[0:1]
-		for i := 0; i < len(template)-1; i++ {
-			currentPair := template[i : i+2]
-			newElement := insertionRules[currentPair]
-			currentPolymer = currentPolymer + newElement + currentPair[1:2]
-			occurrenceMap[newElement]++
+	for i := 0; i < 40; i++ {
+		newPolymerMap := make(map[string]int)
+		for polymer, count := range polymerMap {
+			newElement := insertionRules[polymer]
+			newPolymerMap[string(polymer[0])+newElement] += count
+			newPolymerMap[newElement+string(polymer[1])] += count
+			occurrenceMap[newElement] += count
 		}
-
-		template = currentPolymer
-		//fmt.Printf("Occurrence map: %v\n", occurrenceMap)
-		//fmt.Printf("Step %d: %s\nLength: %d", step, template, len(template))
+		polymerMap = newPolymerMap
+    fmt.Printf("%d - Polymer map: %v\n", i, polymerMap)
+    fmt.Printf("Occurrence map: %v\n", occurrenceMap)
+    fmt.Printf("polymenr length: %d\n", getLength(occurrenceMap))
 	}
 
-	max, min := -1, int(1e10)
+	max, min := -1, math.MaxInt
 	for _, count := range occurrenceMap {
 		if count < min {
-      min = count
+			min = count
 		}
 
-    if count > max {
-      max = count
-    }
+		if count > max {
+			max = count
+		}
 	}
 
-  fmt.Printf("\nPart1: %d\n", max - min)
+	fmt.Printf("\nPart1: %d\n", max-min)
 }
